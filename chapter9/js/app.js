223f7a7a187d2f3301deb5b97dfc2acc69051aeb
ElmerTheOne <lovaldjohan@gmail.com>
2019-04-13 23:58:20 +0200

    Learning new stuff

--- chapter9/js/app.js
+++ chapter9/js/app.js
@@ -1,7 +1,50 @@
 "use strict";
+var audioContext = new AudioContext();
+
 window.onload = function() {
-  var playButton = document.getElementById("play-button");
+  var playButton = document.getElementById("on-off");
+  var osc = false;
+  var freqSliderVal = document.getElementsByTagName("input")[1].value;
+  var selectedWaveform = "sine";
+  var waveformTypes = document.getElementsByTagName("li");
+
+  function select() {
+    for (var i = 0; i < waveformTypes.length; i++) {
+      waveformTypes[i].classList.remove("selected-waveform");
+    }
+    var selectedWaveformElement = document.getElementById(this.id);
+    selectedWaveform = document.getElementById(this.id).id;
+    console.log(selectedWaveform); // Outputs id
+
+    osc.type = selectedWaveform;
+    selectedWaveformElement.classList.add("selected-waveform");
+  }
+
+  for (var i = 0; i < waveformTypes.length; i++) {
+    waveformTypes[i].addEventListener("click", select);
+  }
   playButton.addEventListener("click", function() {
-    alert("You've been hit by, you've been struck by. Truck.");
+    if (!osc) {
+      console.log(freqSliderVal);
+      osc = audioContext.createOscillator();
+      osc.type = selectedWaveform;
+      osc.frequency.value = freqSliderVal;
+      osc.connect(audioContext.destination);
+      osc.start(audioContext.currentTime);
+      playButton.value = "STOP";
+    } else {
+      osc.stop(audioContext.currentTime);
+      osc = false;
+      playButton.value = "START";
+    }
   });
+  setInterval(function() {
+    if (!osc) {
+      console.log("Oscillator is stopped. Waiting for oscillator to start.");
+    } else {
+      freqSliderVal = document.getElementsByTagName("input")[1].value;
+      osc.frequency.value = freqSliderVal;
+      console.log("Oscillator is playing. Frequency value is " + freqSliderVal);
+    }
+  }, 400);
 };
